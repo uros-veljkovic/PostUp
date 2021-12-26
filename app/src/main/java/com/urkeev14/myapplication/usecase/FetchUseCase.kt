3a63bd13860502154a4feed3ffbd27e0c12d3919
Uros Veljkovic <uros.veljkovic@gmail.com>
2021-12-26 15:18:37 +0100

    Rename usecase

--- app/src/main/java/com/urkeev14/myapplication/usecase/FetchUseCase.kt
+++ app/src/main/java/com/urkeev14/myapplication/usecase/FetchUseCase.kt
@@ -1,16 +1,15 @@
 package com.urkeev14.myapplication.usecase
 
 import com.urkeev14.myapplication.data.source.remote.RemoteDataSource
-import com.urkeev14.myapplication.utils.network.RepositoryResponse
 import com.urkeev14.myapplication.utils.DataMapper
+import com.urkeev14.myapplication.utils.network.RepositoryResponse
 import com.urkeev14.myapplication.utils.ui.UiState
 import javax.inject.Inject
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
 
-class FetchAllUseCase<Dto, Entity>
+class FetchUseCase<DtoId, Dto, Entity>
 @Inject constructor(
-    private val remoteDataSource: RemoteDataSource<Dto>,
+    private val remoteDataSource: RemoteDataSource<DtoId, Dto>,
     private val mapper: DataMapper<Dto, Entity>,
 ) {
     /**
@@ -21,6 +20,20 @@ class FetchAllUseCase<Dto, Entity>
     operator fun invoke() = flow {
         emit(UiState.Loading())
         when (val response = remoteDataSource.getAll()) {
+            is RepositoryResponse.Failure -> emit(UiState.Error(throwable = response.throwable))
+            is RepositoryResponse.Success -> emit(UiState.Success(mapper.map(response.data)))
+        }
+    }
+
+    /**
+     * Fetch single [Dto] from [RemoteDataSource] and return it as [Entity]
+     *
+     * @param id id of [Dto]
+     * @return [UiState.Success] if data is successfully fetched, else [UiState.Error]
+     */
+    operator fun invoke(id: DtoId) = flow {
+        emit(UiState.Loading())
+        when (val response = remoteDataSource.getOne(id)) {
             is RepositoryResponse.Failure -> emit(UiState.Error(throwable = response.throwable))
             is RepositoryResponse.Success -> emit(UiState.Success(mapper.map(response.data)))
         }
