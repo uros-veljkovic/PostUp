9cb32a1389d1c9bd69824c77a4914d0f8c4e8604
Uros Veljkovic <uros.veljkovic@gmail.com>
2021-12-27 09:33:23 +0100

    Handle toolbar back button and introduce navigation animations

--- app/src/main/java/com/urkeev14/myapplication/feature/MainActivity.kt
+++ app/src/main/java/com/urkeev14/myapplication/feature/MainActivity.kt
@@ -2,6 +2,11 @@ package com.urkeev14.myapplication.feature
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import com.urkeev14.myapplication.R
 import com.urkeev14.myapplication.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -9,9 +14,23 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupNavigation()
     }
+
+    private fun setupNavigation() {
+        appBarConfiguration = AppBarConfiguration.Builder(R.id.postsFragment).build()
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
+        NavigationUI.setupActionBarWithNavController(this, navHostFragment.navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navHostFragment.navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
