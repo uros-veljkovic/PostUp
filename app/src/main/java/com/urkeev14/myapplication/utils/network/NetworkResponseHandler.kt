3e3a9fbe1f02fc6d5bd4ee3ba7396eac95eaa7b7
Uros Veljkovic <uros.veljkovic@gmail.com>
2021-12-27 17:10:25 +0100

    Introduce method for handling flowable database actions

--- app/src/main/java/com/urkeev14/myapplication/utils/network/NetworkResponseHandler.kt
+++ app/src/main/java/com/urkeev14/myapplication/utils/network/NetworkResponseHandler.kt
@@ -1,12 +1,24 @@
 package com.urkeev14.myapplication.utils.network
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+
 /**
- * Executes database action and wraps it's result into [RepositoryResponse]
+ * Handles database method that return [Flow] of [Entity] and wraps [Entity] with [RepositoryResponse]
  *
  * @param Entity data loaded from database
- * @param load database action being processed
- * @return [RepositoryResponse.Success] if database action is successful, otherwise [RepositoryResponse.Failure]
+ * @param load flowable database action being processed
+ * @return [Flow] of [RepositoryResponse.Success] if database action is successful, otherwise [Flow] of [RepositoryResponse.Failure]
  */
+fun <Entity> executeFlowableDatabaseAction(load: () -> Flow<Entity>): Flow<RepositoryResponse<Entity>> {
+    return load().map {
+        RepositoryResponse.Success(it)
+    }.catch { exception ->
+        RepositoryResponse.Failure<RepositoryResponse<Entity>>(Throwable(exception.message))
+    }
+}
+
 suspend fun <Entity> executeDatabaseAction(load: suspend () -> Entity): RepositoryResponse<Entity> {
     return try {
         val result = load()
