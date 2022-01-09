fdb4a83d31178070b14f1332e886e53ff4933596
Uros Veljkovic <uros.veljkovic@gmail.com>
2022-01-09 17:19:03 +0100

    Modify function for handling http response to suite `Ktor` client

--- app/src/main/java/com/urkeev14/myapplication/utils/network/NetworkActionHandler.kt
+++ app/src/main/java/com/urkeev14/myapplication/utils/network/NetworkActionHandler.kt
@@ -2,13 +2,11 @@ package com.urkeev14.myapplication.utils.network
 
 import android.content.Context
 import com.urkeev14.myapplication.R
-import kotlinx.coroutines.delay
-import retrofit2.Response
+import timber.log.Timber
 import java.io.IOException
 import javax.inject.Inject
 
 class NetworkActionHandler @Inject constructor(private val context: Context) {
-
     /**
      * Executes network action and wraps it's result into [RepositoryResponse]
      *
@@ -16,19 +14,17 @@ class NetworkActionHandler @Inject constructor(private val context: Context) {
      * @param load network action being processed
      * @return [RepositoryResponse.Success] if HTTP response is successful, otherwise [RepositoryResponse.Failure]
      */
-    suspend fun <DTO> execute(load: suspend () -> Response<DTO>): RepositoryResponse<DTO> {
+    suspend fun <DTO> execute(load: suspend () -> DTO): RepositoryResponse<DTO> {
         return try {
-            // Note: For purpose of UX, delaying for 1 second
-            delay(HALF_A_SECOND)
-            load().let { response ->
-                if (response.isSuccessful) {
-                    RepositoryResponse.Success(response.body()!!)
-                } else {
-                    RepositoryResponse.Failure(Throwable(response.message()))
-                }
+            load().let { responseData ->
+                RepositoryResponse.Success(responseData)
             }
         } catch (e: IOException) {
+            Timber.d(e)
             RepositoryResponse.Failure(Throwable(context.getString(R.string.error_network)))
+        } catch (e: Exception) {
+            Timber.d(e)
+            RepositoryResponse.Failure(e)
         }
     }
 
